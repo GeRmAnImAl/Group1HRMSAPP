4861e52755a1dc92a7a8706abe0747ebacc2ca7d
AJ Germani <germani.anthony77@gmail.com>
2023-07-18 18:57:37 -0400

    Create and implement EmployeeRepository Also created class shells for employee service

---
+++ src/main/java/com/example/group1hrmsapp/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.group1hrmsapp.repository;
+
+import com.example.group1hrmsapp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Override
+    Optional<Employee> findById(Long id);
+}
---
+++ src/main/java/com/example/group1hrmsapp/service/EmployeeServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.group1hrmsapp.service;
+
+public class EmployeeServiceImpl {
+}
