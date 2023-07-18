89a37d5a1a98f361fb70585c63c4a87a66347270
AJ Germani <germani.anthony77@gmail.com>
2023-07-17 21:00:53 -0400

    Initial Commit Created project and connected it to the AWS database created for it.

---
+++ src/main/java/com/example/group1hrmsapp/Group1HrmsappApplication.java
@@ -0,0 +1,13 @@
+package com.example.group1hrmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group1HrmsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group1HrmsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/group1hrmsapp/Group1HrmsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group1hrmsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group1HrmsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
