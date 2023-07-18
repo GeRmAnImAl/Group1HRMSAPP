ee2b615785d2aba9dd173ff88acccdf395c51a46
AJ Germani <germani.anthony77@gmail.com>
2023-07-18 19:16:25 -0400

    Implement EmployeeService EmployeeServiceImpl The interface and class have both been implemented. Relevant methods have been created and given functionality.

--- src/main/java/com/example/group1hrmsapp/service/EmployeeService.java
+++ src/main/java/com/example/group1hrmsapp/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.example.group1hrmsapp.service;
 
+import com.example.group1hrmsapp.model.Employee;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
 public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    Employee getEmployeeById(long id);
+    void saveEmployee(Employee employee);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 }
