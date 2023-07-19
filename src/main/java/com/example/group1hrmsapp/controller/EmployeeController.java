49069c8fcefe3929d556fdb40ee6cb6e3d579ac2
AJ Germani <germani.anthony77@gmail.com>
2023-07-18 20:54:07 -0400

    Create and Implement EmployeeController Create the EmployeeController class and implement functionality for it.

---
+++ src/main/java/com/example/group1hrmsapp/controller/EmployeeController.java
@@ -0,0 +1,71 @@
+package com.example.group1hrmsapp.controller;
+
+import com.example.group1hrmsapp.model.Employee;
+import com.example.group1hrmsapp.repository.EmployeeRepository;
+import com.example.group1hrmsapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employeelist")
+    public String viewEmployeePage(Model model){
+        model.addAttribute("listEmployees", employeeService.getAllEmployees());
+        return "employee_list";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/employeeList";
+    }
+
+    @GetMapping("/showUpdateEmployeeForm/{id}")
+    public String showUpdateEmployeeForm(@PathVariable(value = "id") long id, Model model){
+        Employee employee = employeeService.getEmployeeById(id);
+
+        model.addAttribute("employee", employee);
+        return "update_employee";
+    }
+
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") long id){
+        this.employeeService.deleteEmployeeById(id);
+        return "redirect:/employeeList";
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo, @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir, Model model){
+        int pageSize = 5;
+        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List<Employee> listEmployees = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+        model.addAttribute("listEmployees", listEmployees);
+        return "employee_list";
+    }
+}
